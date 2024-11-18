56ef25af308081143ab3a3b1fb4983d75dc10f0b
calvintj <calvinhendra330@gmail.com>
2024-11-18 12:36:56 +0700

    about us

--- resources/views/features/aboutUs.blade.php
+++ resources/views/features/aboutUs.blade.php
@@ -4,4 +4,57 @@
 
 @section('content')
     
+<section class="aboutUs section container py-5">
+    <div class="row align-items-center mb-4" data-aos="fade-right">
+      <div class="col-md-6">
+        <h2 class="mb-3">What is MakanGak</h2>
+        <p>
+          In today's fast-paced world, navigating the diverse culinary landscape can be overwhelming. With countless dining options and varying preferences, choosing the perfect restaurant or meal can feel like a daunting task. MAKANGAK was created to simplify this process, serving as your personalized culinary companion. We understand that every individual has unique tastes and dietary needs. Our platform is designed to connect you with dining experiences that truly resonate with you.
+        </p>
+      </div>
+    </div>
+  
+    <div class="row text-center mb-5">
+      <div class="col">
+        <h1 class="aboutUs-text">Your Culinary Compass: Guiding You to Delicious Discoveries</h1>
+        <p>Find Your Flavor, Your Way. Your Culinary Compass.</p>
+      </div>
+    </div>
+  
+    <div class="row g-4">
+      <div class="col-md-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Our Approach</h2>
+            <p class="card-text">
+              Our approach revolves around providing an intuitive search experience, delivering personalized recommendations based on your preferences and past choices, empowering you with authentic reviews and ratings from fellow food enthusiasts, and fostering connections between discerning diners and local businesses.
+            </p>
+          </div>
+        </div>
+      </div>
+  
+      <div class="col-md-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Our Vision</h2>
+            <p class="card-text">
+              We envision MAKANGAK as a thriving community of food lovers, united by a passion for exploration and discovery. We are dedicated to enriching your dining experiences, fostering meaningful connections within the culinary world, and empowering local businesses.
+            </p>
+          </div>
+        </div>
+      </div>
+  
+      <div class="col-md-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Join Us on Your Culinary Journey</h2>
+            <p class="card-text">
+              Download MAKANGAK today and embark on a culinary adventure tailored to your tastes. Whether you're a seasoned foodie or simply seeking a convenient meal, let us guide you towards your next unforgettable dining experience.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>  
+
 @endsection
